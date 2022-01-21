902df26208007fd3a50b4fc50f59a91b1834ac0b
7777-Katib <85574228+7777-Katib@users.noreply.github.com>
2022-01-21 17:41:39 +0600

    gh

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log(45);
+document.getElementById("case-increase").addEventListener("click",function(){
+    const caseInput=document.getElementById("case-count");
+    const caseCount=parseInt(caseInput.value) 
+    console.log(caseCount);
+    
+})
